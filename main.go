3cc089dcd1ae70de6946f79c83dc6103abaeff66
argnta <awikwokgit71@gmail.com>
2026-01-10 07:32:44 +0700

    Add Func Main

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"Event-Driven-Notification-Service/handlers"
+	"Event-Driven-Notification-Service/worker"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Loading configuration...")
+
+	worker.StartWorkers(5)
+
+	http.HandleFunc("/events", handlers.MainHandlers)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+}
